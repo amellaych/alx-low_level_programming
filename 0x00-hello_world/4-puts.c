b86be1475dd90e66573b4c93d63f62d4c2671c63
abd errahmane <Abdorahmane@gmail.com>
2023-03-16 19:15:52 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that prints a line with function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programing is like building a multilingual puzzle");
+return (0);
+}
