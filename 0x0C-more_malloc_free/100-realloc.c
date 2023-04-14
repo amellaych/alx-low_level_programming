0e8c513b459dd6681285df3224bd799e89701a53
abd errahmane <Abdorahmane@gmail.com>
2023-04-13 18:06:22 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,8 +4,8 @@
 /**
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
+ * @old: size of the allocated memory for ptr
+ * @new: new size of the new memory block
  *
  * Return: pointer to the newly allocated memory block
  */
