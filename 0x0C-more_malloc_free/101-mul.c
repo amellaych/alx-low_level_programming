f98062af654f6fd69d3b8064c2d1cbe5395e52dd
abd errahmane <Abdorahmane@gmail.com>
2023-04-13 18:27:56 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,11 +12,13 @@
  */
 int is_digit(char *s)
 {
-	while (*s)
+	int i = 0;
+
+	while (s[i])
 	{
-		if (*s < '0' || *s > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		s++;
+		i++;
 	}
 	return (1);
 }
@@ -30,9 +32,12 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
-	while (s[i++])
-		;
-	return (i - 1);
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
@@ -53,9 +58,10 @@ void errors(void)
  */
 int main(int argc, char *argv[])
 {
-	char *s1 = argv[1], *s2 = argv[2];
+	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
@@ -66,7 +72,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1--; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -85,11 +91,11 @@ int main(int argc, char *argv[])
 		if (result[i])
 			a = 1;
 		if (a)
-			putchar(result[i] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
-		putchar('0');
-	putchar('\n');
+		_putchar('0');
+	_putchar('\n');
 	free(result);
 	return (0);
 }
