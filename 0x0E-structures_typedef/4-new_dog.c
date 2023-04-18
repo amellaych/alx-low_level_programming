de71a666554b18a2fbc4df71c45a612e88c357b9
abd errahmane <Abdorahmane@gmail.com>
2023-04-17 18:34:03 -0700

    taak 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,39 +11,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *dog;
-        int i, name_len, owner_len;
+	dog_t *dog;
+	int i, name_len, owner_len;
 
-        dog = malloc(sizeof(dog_t));
-        if (dog == NULL)
-                return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-        for (name_len = 0; name[name_len]; name_len++)
-                ;
-        for (owner_len = 0; owner[owner_len]; owner_len++)
-                ;
+	for (name_len = 0; name[name_len]; name_len++)
+		;
+	for (owner_len = 0; owner[owner_len]; owner_len++)
+		;
 
-        dog->name = malloc(name_len + 1);
-        if (dog->name == NULL)
-        {
-                free(dog);
-                return (NULL);
-        }
+	dog->name = malloc(name_len + 1);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
-        dog->owner = malloc(owner_len + 1);
-        if (dog->owner == NULL)
-        {
-                free(dog->name);
-                free(dog);
-                return (NULL);
-        }
+	dog->owner = malloc(owner_len + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
 
-        for (i = 0; i <= name_len; i++)
-                dog->name[i] = name[i];
-        for (i = 0; i <= owner_len; i++)
-                dog->owner[i] = owner[i];
+	for (i = 0; i <= name_len; i++)
+		dog->name[i] = name[i];
+	for (i = 0; i <= owner_len; i++)
+		dog->owner[i] = owner[i];
 
-        dog->age = age;
+	dog->age = age;
 
-        return (dog);
+	return (dog);
 }
