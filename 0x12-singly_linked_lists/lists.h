e2867207a765e2a95fd618de561f9d1f5d3b6bec
amellaych <Abdorahmane2001@Gmail.com>
2023-04-28 17:44:00 -0700

    task 0

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct list_s - linked list
+ * @str: string - (malloc'ed string)
+ * @len: length string
+ * @next: points to next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
+
+/* Function prototypes */
+size_t print_list(const list_t *h);
+
+#endif 
